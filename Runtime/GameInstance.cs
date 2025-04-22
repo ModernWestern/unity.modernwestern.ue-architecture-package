e0598740514e4debe9e529da7795ecaa5d739ec6
Lobsang S. <lobsangwhite@gmail.com>
2025-04-21 22:20:19 -0500

    chore(feat): unload the same scene when possible

--- Runtime/GameInstance.cs
+++ Runtime/GameInstance.cs
@@ -3,9 +3,9 @@ namespace UEArchitecture
     using TMPro;
     using System;
     using UnityEngine;
+    using System.Collections;
     using System.Collections.Generic;
     using UnityEngine.SceneManagement;
-
     using GS = GameState;
     using PS = PlayerState;
     using GE = GameEvents;
@@ -13,19 +13,21 @@ namespace UEArchitecture
     public abstract class GameInstanceBase : MonoBehaviour
     {
         protected static Func<(GS gameState, PS playerState, GE gameEvents)> PersistentData;
-        
+
         protected readonly static Stack<string> SceneStack = new();
-        
+
         protected static string UnRootedSceneName;
 
         public static event Action<string> OnSceneChanged;
-        
+
         public static bool Exists { get; private set; }
-        
+
         [field: SerializeField, HideInInspector]
         public string SelectedScene { get; private set; }
 
         protected abstract void Construct();
+
+        private static Action<IEnumerator> _coroutineRunner;
 
         protected virtual void Awake()
         {
@@ -34,6 +36,8 @@ namespace UEArchitecture
                 LoadScene(SelectedScene, false);
             }
 
+            _coroutineRunner += ie => StartCoroutine(ie);
+
             Exists = true;
 
             Construct();
@@ -41,57 +45,73 @@ namespace UEArchitecture
 
         public static void LoadScene(string sceneName, bool unloadPreviousScene = true)
         {
-            SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
-
             if (unloadPreviousScene && SceneStack.Count > 0)
             {
-                SceneManager.UnloadSceneAsync(SceneStack.Pop());
+                var operation = SceneManager.UnloadSceneAsync(SceneStack.Pop());
+
+                _coroutineRunner(OperationProcess(operation, Handler));
             }
             else
             {
                 SceneStack.Clear();
             }
 
-            OnSceneChanged?.Invoke(sceneName);
-            
-            SceneStack.Push(sceneName);
+            return;
+
+            void Handler()
+            {
+                SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+                OnSceneChanged?.Invoke(sceneName);
+
+                SceneStack.Push(sceneName);
+            }
+        }
+
+        private static IEnumerator OperationProcess(AsyncOperation operation, Action onComplete)
+        {
+            while (!operation.isDone)
+            {
+                yield return null;
+            }
+
+            onComplete?.Invoke();
         }
 
         public static void RootScene()
         {
             UnRootedSceneName = SceneManager.GetActiveScene().name;
-            
+
             SceneStack.Push(UnRootedSceneName);
-            
+
             LoadScene("PersistentLevel");
-            
+
             UnRootedSceneName = null;
         }
     }
 
     public abstract class GameInstance<TGameState, TPlayerState, TGameEvents> : GameInstanceBase
-    
-    where TGameState   : GS
-    where TPlayerState : PS
-    where TGameEvents  : GE
-    
+        where TGameState : GS
+        where TPlayerState : PS
+        where TGameEvents : GE
+
     {
         [SerializeField]
         protected TGameState gameState;
-        
+
         [SerializeField]
         protected TPlayerState playerState;
-        
+
         [SerializeField]
         protected TGameEvents gameEvents;
-        
+
         [SerializeField]
         protected TMP_Text version;
-        
+
         protected override void Awake()
         {
             PersistentData = () => (gameState, playerState, gameEvents);
-            
+
             if (version)
             {
                 version.text = $"v{Application.version}";
@@ -104,7 +124,7 @@ namespace UEArchitecture
         {
             version.text += $"_{environment}";
         }
-        
+
         public static TGameState GameState => (TGameState)PersistentData().gameState;
 
         public static TPlayerState PlayerState => (TPlayerState)PersistentData().playerState;
@@ -114,9 +134,9 @@ namespace UEArchitecture
         private void OnDestroy()
         {
             playerState.Clear();
-            
+
             gameEvents.Clear();
-            
+
             gameState.Clear();
         }
     }
