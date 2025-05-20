744c6dfdd4d6a36177590739d3620d3fbdf1dfb5
Lobsang S. <lobsangwhite@gmail.com>
2025-05-20 12:55:44 -0500

    chore(feat): wrap templates in namespace if user set 'root namespace' in unity editor

--- Editor/Util/ScriptCreator.cs
+++ Editor/Util/ScriptCreator.cs
@@ -1,7 +1,10 @@
-﻿using System.IO;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
+using System.Linq;
+using System.Reflection;
 
 public static class ScriptCreator
 {
@@ -19,18 +22,14 @@ public static class ScriptCreator
             return;
         }
 
-        // Original template
         var template = File.ReadAllText(fullPath);
 
-        // Replace the placeholders
         template = ReplacePlaceholders(template, api?.Replace(" ", string.Empty));
-        
-        // Temporal unity template
+
         var tempPath = Path.Combine(Path.GetTempPath(), $"{apiName}{scriptName}.cs.txt");
-        
+
         File.WriteAllText(tempPath, template);
 
-        // Asset creation
         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(tempPath, $"{apiName}{scriptName}.cs");
     }
 
@@ -45,7 +44,7 @@ public static class ScriptCreator
         }
 
         var scriptPath = AssetDatabase.GUIDToAssetPath(guid);
-        
+
         var rootDirectory = Path.GetDirectoryName(scriptPath);
 
         if (string.IsNullOrEmpty(rootDirectory))
@@ -55,7 +54,7 @@ public static class ScriptCreator
         }
 
         var packageRoot = Path.GetFullPath(Path.Combine(rootDirectory, ".."));
-        
+
         var fullTemplatePath = Path.Combine(packageRoot, string.Format(TemplateRelativePath, templateName));
 
         return fullTemplatePath.Replace("\\", "/");
@@ -64,59 +63,84 @@ public static class ScriptCreator
     private static string ReplacePlaceholders(string template, string api)
     {
         const string apiPlaceholder = "#API#";
-        
-        return template.Replace(apiPlaceholder, api ?? Application.productName);
+
+        template = template.Replace(apiPlaceholder, api ?? Application.productName);
+
+        var rootNamespace = GetRootNamespace();
+
+        if (string.IsNullOrWhiteSpace(rootNamespace))
+        {
+            return template;
+        }
+
+        SplitUsingsAndBody(template, out var usings, out var body);
+
+        var indentedBody = IndentTemplate(body);
+
+        template = $"{usings}\n\nnamespace {rootNamespace}\n{{\n{indentedBody}\n}}";
+
+        return template;
+    }
+
+    private static void SplitUsingsAndBody(string template, out string usings, out string body)
+    {
+        var lines = template.Split(new[]
+        {
+            "\r\n", "\r", "\n"
+
+        }, StringSplitOptions.None);
+
+        var usingLines = new List<string>();
+
+        var bodyLines = new List<string>();
+
+        var pastUsings = false;
+
+        foreach (var line in lines)
+        {
+            if (!pastUsings && line.TrimStart().StartsWith("using"))
+            {
+                usingLines.Add(line);
+            }
+            else
+            {
+                pastUsings = true;
+                bodyLines.Add(line);
+            }
+        }
+
+        usings = string.Join("\n", usingLines);
+
+        body = string.Join("\n", bodyLines).Trim('\n', '\r');
+    }
+
+
+    private static string IndentTemplate(string content, int indentLevel = 1)
+    {
+        var indent = new string(' ', indentLevel * 4);
+
+        var lines = content.Split(new[]
+        {
+            "\r\n", "\r", "\n"
+
+        }, StringSplitOptions.None);
+
+        var indentedLines = lines.Select(line => string.IsNullOrWhiteSpace(line) ? line : indent + line);
+
+        return string.Join("\n", indentedLines);
+    }
+
+    private static string GetRootNamespace()
+    {
+        var editorSettingsType = typeof(EditorSettings);
+
+        var property = editorSettingsType.GetProperty("projectGenerationRootNamespace", BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Public);
+
+        if (property != null)
+        {
+            return property.GetValue(null) as string;
+        }
+
+        return null;
     }
 }
-
-// Without custom placeholders
-//
-// using System.IO;
-// using System.Linq;
-// using UnityEditor;
-// using UnityEngine;
-//
-// public static class ScriptCreator
-// {
-//     private const string TemplateRelativePath = "../Editor/Templates/{0}Template.txt";
-//     
-//     public static void CreateScriptAsset(string scriptName, string api = null)
-//     {
-//         var fullPath = GetTemplateFullPath(scriptName);
-//
-//         if (!File.Exists(fullPath))
-//         {
-//             Debug.LogError($"Template not found at path: {fullPath}");
-//             return;
-//         }
-//
-//         ProjectWindowUtil.CreateScriptAssetFromTemplateFile(fullPath, $"{Application.productName}{scriptName}.cs");
-//     }
-//
-//     private static string GetTemplateFullPath(string templateName)
-//     {
-//         var guid = AssetDatabase.FindAssets(nameof(ScriptCreator)).FirstOrDefault();
-//
-//         if (string.IsNullOrEmpty(guid))
-//         {
-//             Debug.LogError("Could not locate ScriptCreator script in AssetDatabase.");
-//             return null;
-//         }
-//
-//         var scriptPath = AssetDatabase.GUIDToAssetPath(guid);
-//         
-//         var rootDirectory = Path.GetDirectoryName(scriptPath);
-//
-//         if (string.IsNullOrEmpty(rootDirectory))
-//         {
-//             Debug.LogError("Could not locate the path to the scripts folder.");
-//             return null;
-//         }
-//         
-//         var packageRoot = Path.GetFullPath(Path.Combine(rootDirectory, ".."));
-//         
-//         var fullTemplatePath = Path.Combine(packageRoot, string.Format(TemplateRelativePath, templateName));
-//
-//         return fullTemplatePath.Replace("\\", "/");
-//     }
-// }
